9d698cdb853e6d4442a692e6ce395af3a4487799
Pau Bestard <32491515+wertons@users.noreply.github.com>
2022-10-28 22:04:09 +0200

    Fixed bad config asset route

--- app/Http/Controllers/CurrencyRatesController.php
+++ app/Http/Controllers/CurrencyRatesController.php
@@ -22,7 +22,7 @@ class CurrencyRatesController extends Controller
         if (!self::isCurrencyNameValid($currencyName)) {
             return false;
         }
-        $url = config("coinbaseAPIBaseUrl")."/exchange-rates?currency=" . $currencyName;
+        $url = config("app.coinbaseAPIBaseUrl")."/exchange-rates?currency=" . $currencyName;
         //  Initiate curl
         $ch = curl_init();
         // Will return the response, if false it print the response
@@ -50,7 +50,7 @@ class CurrencyRatesController extends Controller
      */
     static public function getValidCurrencies()
     {
-        $url = config("coinbaseAPIBaseUrl")."/currencies";
+        $url = config("app.coinbaseAPIBaseUrl")."/currencies";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $url);
